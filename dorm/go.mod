2622a8a6fc18342a93899cefa301042c8f5997f8
Wed0n <wed0n1213@gmail.com>
2024-05-31 13:03:33 +0800

    feat: using Multiprocessing to boost compilation

--- dorm/go.mod
+++ dorm/go.mod
@@ -4,7 +4,10 @@ go 1.22.3
 
 replace github.com/cuit9622/dms/common => ../common
 
-require github.com/cuit9622/dms/common v0.0.0
+require (
+	github.com/cuit9622/dms/common v0.0.0
+	google.golang.org/grpc v1.64.0
+)
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
@@ -66,7 +69,6 @@ require (
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
-	google.golang.org/grpc v1.64.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gorm.io/driver/mysql v1.5.6 // indirect
--- util.py
+++ util.py
@@ -1,45 +1,59 @@
 import argparse
+import multiprocessing
 import os
 import subprocess
 
 
-item_list = os.listdir(".")
 ignoreFolders = ["build", "common"]
-folders = []
+item_list = os.listdir(".")
+folders: list[str] = []
 for item in item_list:
-    if not item.startswith('.') and os.path.isdir(os.path.join(".", item)) and item not in ignoreFolders:
+    if not item.startswith('.')\
+            and os.path.isdir(os.path.join(".", item))\
+            and item not in ignoreFolders:
         folders.append(item)
 
 
-def tidy():
-    folders.insert(0, "common")
+def executeProcess(folder: str, command: str):
+    os.chdir(folder)
+    result = subprocess.run(
+        command, shell=True)
+    if result.returncode != 0:
+        raise Exception(f"excute {command} error in {folder}")
+
+
+def execute(folders: list[str], command: str):
+    processes = []
     for folder in folders:
-        os.chdir(folder)
-        result = subprocess.run(
-            "go mod tidy", shell=True)
-        if result.returncode != 0:
-            raise Exception(f"tidy {folder} error")
-        os.chdir('..')
+        process = multiprocessing.Process(target=executeProcess, args=(
+            folder, command))
+        process.start()
+        processes.append(process)
+
+    for process in processes:
+        process.join()
+
+
+def tidy():
+    command = "go mod tidy"
+    executeProcess("common", command)
+    execute(folders, command)
 
 
 def build():
     os.makedirs("build", exist_ok=True)
-    for folder in folders:
-        os.chdir(folder)
-        result = subprocess.run(
-            "go build -o ../build", shell=True)
-        if result.returncode != 0:
-            raise Exception(f"build {folder} error")
-        os.chdir('..')
+    execute(folders, "go build -o ../build")
 
 
-parser = argparse.ArgumentParser(description='Help to build')
-parser.add_argument('action', choices=[
-                    'tidy', 'build'], help='Choose one of the actions')
-args = parser.parse_args()
-if args.action == 'tidy':
-    tidy()
-elif args.action == 'build':
-    build()
-else:
-    raise Exception("unknow arg")
+if __name__ == '__main__':
+    multiprocessing.freeze_support()  # 在Windows下必须调用此函数
+    parser = argparse.ArgumentParser(description='Help to build')
+    parser.add_argument('action', choices=[
+                        'tidy', 'build'], help='Choose one of the actions')
+    args = parser.parse_args()
+    if args.action == 'tidy':
+        tidy()
+    elif args.action == 'build':
+        build()
+    else:
+        raise Exception("unknow arg")

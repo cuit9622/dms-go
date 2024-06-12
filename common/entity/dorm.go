ece48623bf2176b48d212c99487c2a330e2f958c
Wed0n <wed0n1213@gmail.com>
2024-06-12 16:05:31 +0800

    feat: complete get dorm

--- common/entity/dorm.go
+++ common/entity/dorm.go
@@ -1,21 +1,22 @@
 package entity
 
+//go:generate gomodifytags -all -add-tags form,json -add-options json=omitempty -transform camelcase --skip-unexported -w -file $GOFILE
 type Dorm struct {
-	ID             int64  `gorm:"primaryKey;comment:寝室主键"`
-	Name           string `gorm:"not null;comment:寝室名字"`
-	Size           int32  `gorm:"not null;comment:寝室人数"`
-	Floor          int32  `gorm:"not null;comment:寝室所在楼层"`
-	DormBuildingID int64  `gorm:"not null;comment:寝室楼ID"`
-	DormBeds       []DormBed
+	ID             int64     `gorm:"primaryKey;comment:寝室主键" form:"id" json:"id,omitempty"`
+	Name           string    `gorm:"not null;comment:寝室名字" form:"name" json:"name,omitempty"`
+	Size           int32     `gorm:"not null;comment:寝室人数" form:"size" json:"size,omitempty"`
+	Floor          int32     `gorm:"not null;comment:寝室所在楼层" form:"floor" json:"floor,omitempty"`
+	DormBuildingID int64     `gorm:"not null;comment:寝室楼ID" form:"dormBuildingID" json:"dormBuildingID,omitempty"`
+	DormBeds       []DormBed `form:"dormBeds" json:"dormBeds,omitempty"`
 }
 type DormBed struct {
-	ID        int64 `gorm:"primaryKey;comment:床ID"`
-	StudentID int64 `gorm:"not null;comment:床绑定的学生ID"`
-	DormID    int64 `gorm:"not null;comment:寝室ID"`
+	ID        int64 `gorm:"primaryKey;comment:床ID" form:"id" json:"id,omitempty"`
+	StudentID int64 `gorm:"not null;comment:床绑定的学生ID" form:"studentID" json:"studentID,omitempty"`
+	DormID    int64 `gorm:"not null;comment:寝室ID" form:"dormID" json:"dormID,omitempty"`
 }
 type DormBuilding struct {
-	ID    int64  `gorm:"primaryKey;comment:寝室楼ID"`
-	Name  string `gorm:"not null;comment:寝室名字"`
-	Sex   int8   `gorm:"not null;comment:寝室性别 0-女,1-男"`
-	Floor int32  `gorm:"not null;comment:寝室楼层"`
+	ID    int64  `gorm:"primaryKey;comment:寝室楼ID" form:"id" json:"id,omitempty"`
+	Name  string `gorm:"not null;comment:寝室名字" form:"name" json:"name,omitempty"`
+	Sex   int8   `gorm:"not null;comment:寝室性别 0-女,1-男" form:"sex" json:"sex"`
+	Floor int32  `gorm:"not null;comment:寝室楼层" form:"floor" json:"floor,omitempty"`
 }
--- dorm/api/dorm.go
+++ dorm/api/dorm.go
@@ -2,11 +2,13 @@ package api
 
 import (
 	"context"
+	"github.com/cuit9622/dms/common/entity"
 	"github.com/cuit9622/dms/common/model"
 	"github.com/cuit9622/dms/common/pb"
 	"github.com/cuit9622/dms/common/response"
 	"github.com/cuit9622/dms/common/response/errors"
 	"github.com/cuit9622/dms/common/util/grpcUtil"
+	"github.com/cuit9622/dms/common/util/httpClientUtil"
 	"github.com/cuit9622/dms/dorm/client"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
@@ -14,15 +16,30 @@ import (
 	"google.golang.org/protobuf/types/known/anypb"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 	"strconv"
+	"sync"
 )
 
-//go:generate gomodifytags -all -add-tags form -transform camelcase --skip-unexported -w -file $GOFILE
+//go:generate gomodifytags -all -add-tags form,json -add-options json=omitempty -transform camelcase --skip-unexported -w -file $GOFILE
 
 type dormGetRequest struct {
-	DormBuildingID int64 `form:"dormBuildingID"`
-	Floor          int32 `form:"floor"`
-	Page           int32 `form:"page"`
-	PageSize       int32 `form:"pageSize"`
+	DormBuildingID int64 `form:"dormBuildingID" json:"dormBuildingID,omitempty"`
+	Floor          int32 `form:"floor" json:"floor,omitempty"`
+	Page           int32 `form:"page" json:"page,omitempty"`
+	PageSize       int32 `form:"pageSize" json:"pageSize,omitempty"`
+}
+type Dorm struct {
+	entity.Dorm
+	DormBeds []DormBed `form:"dormBeds" json:"dormBeds,omitempty"`
+}
+type DormBed struct {
+	entity.DormBed
+	StudentName string `form:"studentName" json:"studentName,omitempty"`
+	StudentNo   string `form:"studentNo" json:"studentNo,omitempty"`
+}
+type Student struct {
+	Name   string `json:"name,omitempty" form:"name"`
+	Sex    int64  `json:"sex" form:"sex"`
+	StuNum string `json:"stuNum,omitempty" form:"stuNum"`
 }
 
 func getDorms(c *gin.Context) {
@@ -57,7 +74,49 @@ func getDorms(c *gin.Context) {
 	if dst.Dorms == nil {
 		dst.Dorms = []*pb.Dorm{}
 	}
-	pageResult.Records = &dst.Dorms
+
+	wg := sync.WaitGroup{}
+	records := make([]Dorm, len(dst.Dorms))
+	rpcClient := httpClientUtil.New()
+	for i, dorm := range dst.Dorms {
+		record := &records[i]
+		record.ID = dorm.Id
+		record.Name = dorm.Name
+		record.Size = dorm.Size
+		record.Floor = dorm.Floor
+
+		item := dorm
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for _, bed := range item.DormBeds {
+				wg.Add(1)
+				bed := bed
+				go func() {
+					defer wg.Done()
+					student := Student{}
+					err := rpcClient.GetWithPathVariable(
+						"student",
+						"/student",
+						strconv.FormatInt(bed.StudentID, 10),
+						&student)
+					if err != nil {
+						return
+					}
+					record.DormBeds = append(record.DormBeds, DormBed{
+						DormBed: entity.DormBed{
+							ID:        bed.Id,
+							StudentID: bed.StudentID,
+						},
+						StudentName: student.Name,
+						StudentNo:   student.StuNum,
+					})
+				}()
+			}
+		}()
+	}
+	wg.Wait()
+	pageResult.Records = &records
 	response.Success(c, pageResult)
 }
 func updateDorm(c *gin.Context) {
